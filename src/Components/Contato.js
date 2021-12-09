a9ea8f5e34d4797696118c6eef0bb1ba3e4d11a1
Renato Tripi <renatosft@gmail.com>
2021-12-09 18:47:40 -0300

    first commit, aplicação feita durante curso de React da Origamid.

---
+++ src/Components/Contato.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Contato.module.css';
+import foto from '../img/contato.jpg';
+import Head from './Head';
+
+const Contato = () => {
+  return (
+    <section className={`${styles.contato} animeLeft`}>
+      <Head title="Ranek | Contato" description="Entre em contato." />
+      <img src={foto} alt="máquina de escrever" />
+      <div>
+        <h1>Entre em contato</h1>
+        <ul className={styles.dados}>
+          <li>renatosft@gmail.com</li>
+          <li>999999999</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Contato;
---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className={styles.header}>
+      <ul>
+        <li>
+          <NavLink className={styles.link} end to="/">
+            Produtos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={styles.link} to="contato">
+            Contato
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
